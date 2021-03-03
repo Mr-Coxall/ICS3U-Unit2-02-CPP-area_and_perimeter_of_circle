d3bf2d4d657fe97f7e04bf711158a53f83c172f5
Patrick Coxall <mr.coxall@mths.ca>
2021-03-03 18:04:26 +0000

    changed variable names

--- area_and_perimeter_of_circle.cpp
+++ area_and_perimeter_of_circle.cpp
@@ -1,29 +1,29 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
-// This program calculates the area and perimeter of a circle
+// This program calculates the areaOfCircle and perimeterOfCircle of a circle
 //    with user input
 
 #include <iostream>
 #include <cmath>
 
 int main() {
-    // this function calculates the area and perimeter of a circle
-    int radius;
-    int area;
-    int perimeter;
+    // this function calculates the areaOfCircle and perimeterOfCircle of a circle
+    int radiusOfCircle;
+    int areaOfCircle;
+    int perimeterOfCircle;
 
     //input
 	std::cout << "Enter radius of a circle (mm): ";
-    std::cin >> radius;
+    std::cin >> radiusOfCircle;
 
     // process
-    area = M_PI * pow(radius, 2);
-    perimeter = 2 * M_PI * radius;
+    areaOfCircle = M_PI * pow(radiusOfCircle, 2);
+    perimeterOfCircle = 2 * M_PI * radiusOfCircle;
     
     // output
     std::cout << "" << std::endl;
-    std::cout << "Area is " << area << " mm²" << std::endl;
-    std::cout << "Perimeter is " << perimeter << " mm" << std::endl;
+    std::cout << "Area is " << areaOfCircle << " mm²" << std::endl;
+    std::cout << "Perimeter is " << perimeterOfCircle << " mm" << std::endl;
     std::cout << std::endl;
     std::cout << "Done." << std::endl;
 }
